af06858f1baa4a36263afc8f0e954c2966a06744
hobbs-chris <hobbs.chris.m@gmail.com>
2023-08-18 07:41:10 -0500

    Added about_us and contact_us

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -10,10 +10,10 @@ urlpatterns = [
     # name the URL
 
     # path for about view
-    path(route='', view=views.about, name='about_us')
+    path(route='about.html', view=views.about, name='about'),
 
     # path for contact us view
-    path(route='', view=views.contact , name='about_us')
+    path(route='contact.html', view=views.contact, name='contact'),
 
     # path for registration
 
