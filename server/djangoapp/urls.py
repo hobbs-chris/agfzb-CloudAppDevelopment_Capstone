ee6f11162bef04d8202b96a616d891193ec93ea5
hobbs-chris <hobbs.chris.m@gmail.com>
2023-08-21 16:44:25 -0500

    added CarMake and CarModel models

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -19,9 +19,11 @@ urlpatterns = [
     path(route='registration.html', view=views.registration_request, name='registration'),
 
     # path for login
+    path(route='login/',view=views.login_request, name='login'),
 
     # path for logout
-    path('logout/', views.logout_request, name='index'),
+    path('logout/', view=views.logout_request, name='logout'),
+
     path(route='', view=views.get_dealerships, name='index'),
 
     # path for dealer reviews view
--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -1,55 +1,97 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <title>Dealership Review</title>
-    <link
-      rel="stylesheet"
-      href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-    />
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
-    <link
-      href="https://unpkg.com/bootstrap-table@1.18.2/dist/bootstrap-table.min.css"
-      rel="stylesheet"
-    />
-    <script src="https://unpkg.com/bootstrap-table@1.18.2/dist/bootstrap-table.min.js"></script>
-    <script src="https://unpkg.com/bootstrap-table@1.18.2/dist/extensions/filter-control/bootstrap-table-filter-control.min.js"></script>
-  </head>
+from django.shortcuts import render
+from django.http import HttpResponseRedirect, HttpResponse
+from django.contrib.auth.models import User
+from django.shortcuts import get_object_or_404, render, redirect
+# from .models import related models
+# from .restapis import related methods
+from django.contrib.auth import login, logout, authenticate
+from django.contrib import messages
+from datetime import datetime
+import logging
+import json
 
-  <body>
-    <!--Add a nav bar here -->
-    <nav
-      class="navbar"
-      style="background-color: rgb(169, 200, 200); color: white"
-    >
-    <div class="container-fluid">
-        <div class="navbar-header">
-              <a class="navbar-brand" href="{% url 'djangoapp:about' %}">Dealership Review</a>
-        </div>
-        <ul class="nav navbar-nav navbar-right">
-            {% if user.is_authenticated %}
-            <li>
-                <a class="btn btn-link" href="#">{{ user.first_name }}({{ user.username }})</a>
-                <a class="btn btn-link" href="{% url 'djangoapp:logout' %}">Logout</a>
-            </li>
-            {% else %}
-            <li>
-                <form class="form-inline" action="{% url 'djangoapp:login' %}" method="post">
-                    {% csrf_token %}
-                    <div class="input-group">
-                        <input type="text" class="form-control" placeholder="Username" name="username" >
-                        <input type="password" class="form-control" placeholder="Username" name="psw" >
-                        <button class="btn btn-primary" type="submit">Login</button>
-                        <a class="btn btn-link" href="{% url 'djangoapp:registration' %}">Sign Up</a>
-                    </div>
-                </form>
-            </li>
-            {% endif %}
-        </ul>
-    </div>
-    </nav>
-    <!--Add a dealer table here -->
-  </body>
-</html>
+# Get an instance of a logger
+logger = logging.getLogger(__name__)
+
+
+# Create your views here.
+
+
+# Create an `about` view to render a static about page
+def about(request):
+    if request.method == "GET":
+        return render(request, "djangoapp/about.html")
+# ...
+
+
+# Create a `contact` view to return a static contact page
+def contact(request):
+    if request.method == "GET":
+        return render(request, "djangoapp/contact.html")
+    
+# Create a `login_request` view to handle sign in request
+def login_request(request):
+    # Handles POST request
+    if request.method == "POST":
+        # Get username and password
+        username = request.POST['username']
+        password = request.POST['psw']
+        # Check if credentials exist
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            # If credentials are valid, call login method
+            login(request, user)
+            return redirect('djangoapp:index')
+        else:
+            messages.warning(request, "Invalid username or password.")
+            return render('djangoapp/index.html')   
+# ...
+
+# Create a `logout_request` view to handle sign out request
+def logout_request(request):
+    logout(request)
+    return redirect('djangoapp:index')
+# ...
+
+# Create a `registration_request` view to handle sign up request
+def registration_request(request):
+    context = {}
+    if request.method == 'GET':
+        return render(request, 'djangoapp/registration.html', context)
+    elif request.method == 'POST':
+        # Check if user exists
+        username = request.POST['username']
+        password = request.POST['psw']
+        first_name = request.POST['firstname']
+        last_name = request.POST['lastname']
+        user_exist = False
+        try:
+            User.objects.get(username=username)
+            user_exist = True
+        except:
+            logger.error("New user")
+        if not user_exist:
+            user = User.objects.create_user(username=username, first_name=first_name, last_name=last_name,
+                                            password=password)
+            login(request, user)
+            return redirect("djangoapp:index")
+        else:
+            context['message'] = "User already exists."
+            return render(request, 'djangoapp/registration.html', context)
+# ...
+
+# Update the `get_dealerships` view to render the index page with a list of dealerships
+def get_dealerships(request):
+    context = {}
+    if request.method == "GET":
+        return render(request, 'djangoapp/index.html', context)
+
+
+# Create a `get_dealer_details` view to render the reviews of a dealer
+# def get_dealer_details(request, dealer_id):
+# ...
+
+# Create a `add_review` view to submit a review
+# def add_review(request, dealer_id):
+# ...
+
